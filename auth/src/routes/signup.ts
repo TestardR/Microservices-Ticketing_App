a5f5aca7c07c2f38c4e4af90b311e9f7bc570b2a
unknown <rmtestard@gmail.com>
2020-05-24 20:44:45 +0200

    I have added a middleware to handle errors

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 
+import { DatabaseConnectionError } from './../errors/database-connection-error';
+import { RequestValidationError } from './../errors/request-validation-error';
+
 const router = express.Router();
 
 router.post(
@@ -16,10 +19,11 @@ router.post(
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      throw new RequestValidationError(errors.array());
     }
 
     const { email, password } = req.body;
+    throw new DatabaseConnectionError();
 
     console.log('Creating a user...');
 
