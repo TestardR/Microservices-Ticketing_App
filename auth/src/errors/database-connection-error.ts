9160429d5b0b2e0e188a39d89a106cd2b1818631
unknown <rmtestard@gmail.com>
2020-05-26 21:16:43 +0200

    I have added customError abstract class to handle different kinds of Errors

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,10 +1,16 @@
-export class DatabaseConnectionError extends Error {
-  reason = 'Error connection to database';
+import { CustomError } from "./custom-errors";
+
+export class DatabaseConnectionError extends CustomError {
+  statusCode = 500;
+  reason = 'Error connecting to database';
 
   constructor() {
-    super();
-
+    super('Error connecting to database');
     // Only because we are extending a built in class
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: this.reason }];
   }
 }
